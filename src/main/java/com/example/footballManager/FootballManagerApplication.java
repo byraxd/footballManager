f62256f3296665548ef142fae688234afcf46ee3
byraxd <byraxd@gmail.com>
2024-12-31 14:07:00 +0200

    Initial commit

---
+++ src/main/java/com/example/footballManager/FootballManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.footballManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/footballManager/FootballManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.footballManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
